2700b614a581b22ce6423996129d98360dc9596f
Priyanka-code <priyankasarkate@gmail.com>
2019-07-22 11:23:14 +0530

    FlightController

---
+++ src/main/java/com/hcl/flight/controller/FlightController.java
@@ -0,0 +1,13 @@
+package com.hcl.flight.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/flights")
+public class FlightController {
+	
+	
+	
+
+}
