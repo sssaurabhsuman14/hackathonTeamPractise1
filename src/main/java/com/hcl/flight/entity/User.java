3b88db9e1b26ea1b6d1d8fc851ef96f1a69f15c4
monikanidhisingh <monikanidhisingh@gmail.com>
2019-07-22 12:30:35 +0530

    merge conflicts

---
+++ src/main/java/com/hcl/flight/entity/User.java
@@ -0,0 +1,50 @@
+package com.hcl.flight.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="user")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id", nullable=false)
+	private Long userId;
+	
+	@Column(name = "user_name", nullable=false)
+	private String userName;
+	
+	@Column(name = "password", nullable=false)
+	private String password;
+	
+	@Column(name = "user_age", nullable=false)
+	private Integer userAge;
+	
+	@Column(name = "user_gender", nullable=false)
+	private Long userGender;
+	
+	@Column(name = "user_role", nullable=false)
+	private String userRole;
+	
+	
+}
