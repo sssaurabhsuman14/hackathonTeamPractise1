249d482524ce8e5217baa4ed928dcebbb7b01d3c
Sagar Gaikwad <sagargaikwad966@gmail.com>
2019-07-22 12:34:21 +0530

    flight entity

--- src/main/java/com/hcl/flight/repository/FlightRepository.java
+++ src/main/java/com/hcl/flight/repository/FlightRepository.java
@@ -3,6 +3,8 @@ package com.hcl.flight.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hcl.flight.entity.Flight;
+
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long>{
 
